48983788ccabe9d0be50b82c8f8b172c84399ddc
Mike Long <mblongii@gmail.com>
2013-01-09 00:08:13 -0800

    separating console message (greeting) to separate file

---
+++ public/javascripts/greeting.js
@@ -0,0 +1,14 @@
+(function() {
+
+  jQuery(function() {
+    var greet, person;
+    if ($('.chat').length > 0 && $('.chat .name').attr('data-person').length > 0) {
+      person = $('.name').attr('data-person');
+      greet = function(person) {
+        return console.log('Hello, ' + ("" + person) + '!');
+      };
+      return greet(person);
+    }
+  });
+
+}).call(this);
